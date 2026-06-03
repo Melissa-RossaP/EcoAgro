be2e8eec383cf33f100d18eedd87a48dc7b973df
Meli 🌸 <melissarossap@gmail.com>
2026-06-03 16:00:47 -0300

    Add files via upload

---
+++ comentarios.js
@@ -0,0 +1,75 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    orderBy,
+    onSnapshot,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0WFKpk24pwVy4PdrGR_WW-uAhNKg3Y7U",
+    authDomain: "comentariosecoagro.firebaseapp.com",
+    projectId: "comentariosecoagro",
+    storageBucket: "comentariosecoagro.firebasestorage.app",
+    messagingSenderId: "938966924519",
+    appId: "1:938966924519:web:e582f72204bdfb39367642"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.enviarComentario = async function () {
+
+    const nome = document.getElementById("nome").value.trim();
+    const mensagem = document.getElementById("mensagem").value.trim();
+
+    if (!nome || !mensagem) {
+        alert("Preencha todos os campos.");
+        return;
+    }
+
+    try {
+
+        await addDoc(collection(db, "comentarios"), {
+            nome,
+            mensagem,
+            data: serverTimestamp()
+        });
+
+        document.getElementById("nome").value = "";
+        document.getElementById("mensagem").value = "";
+
+    } catch (erro) {
+        console.error(erro);
+        alert("Erro ao enviar comentário.");
+    }
+};
+
+const lista = document.getElementById("listaComentarios");
+
+const q = query(
+    collection(db, "comentarios"),
+    orderBy("data", "desc")
+);
+
+onSnapshot(q, (snapshot) => {
+
+    lista.innerHTML = "";
+
+    snapshot.forEach((doc) => {
+
+        const comentario = doc.data();
+
+        lista.innerHTML += `
+            <div class="comentario">
+                <div class="nome">${comentario.nome}</div>
+                <p>${comentario.mensagem}</p>
+            </div>
+        `;
+    });
+
+});
